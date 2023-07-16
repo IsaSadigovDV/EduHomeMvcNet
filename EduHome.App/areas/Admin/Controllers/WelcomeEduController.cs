6449b81edc8dba16827d642a0f69302fc5082ec9
IsaSadigovDV <isans@code.edu.az>
2023-07-16 11:33:31 +0400

    welcomeedu update added

--- EduHome.App/areas/Admin/Controllers/WelcomeEduController.cs
+++ EduHome.App/areas/Admin/Controllers/WelcomeEduController.cs
@@ -1,12 +1,120 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.App.Context;
+using EduHome.App.Extentions;
+using EduHome.App.Helpers;
+using EduHome.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EduHome.App.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class WelcomeEduController : Controller
     {
-        public IActionResult Index()
+        private readonly EduHomeDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public WelcomeEduController(EduHomeDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<WelcomeEdu> welcomeEdus = await _context.WelcomeEdus
+                .Where(x => !x.IsDeleted).ToListAsync();
+            return View(welcomeEdus);
+        }
+
+        public async Task<IActionResult> Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(WelcomeEdu welcomeEdu)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if(welcomeEdu == null)
+            {
+                return NotFound();
+            }
+
+            if(welcomeEdu.FormFile == null)
+            {
+                ModelState.AddModelError("Formfile", "This field is required only image");
+            }
+            if (!Helper.IsImage(welcomeEdu.FormFile))
+            {
+                ModelState.AddModelError("Formfile", "File type must be image");
+            }
+            if (!Helper.IsSizeOk(welcomeEdu.FormFile,2))
+            {
+                ModelState.AddModelError("Formfile", "File size can not be more than 2 mb");
+            }
+
+            welcomeEdu.Image = welcomeEdu.FormFile.CreateImage(_env.WebRootPath, "/img/about");
+            welcomeEdu.CreatedDate = DateTime.UtcNow.AddHours(4);
+            await _context.AddAsync(welcomeEdu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            WelcomeEdu? welcomeEdu = await _context.WelcomeEdus
+                    .Where(x => !x.IsDeleted && x.Id == id).FirstOrDefaultAsync();
+            if(welcomeEdu == null)
+            {
+                return NotFound();
+            }
+            return View(welcomeEdu);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, WelcomeEdu welcomeEdu)
+        {
+            WelcomeEdu? updatedWelcomeEdu = await _context.WelcomeEdus
+                   .Where(x => !x.IsDeleted && x.Id == id).FirstOrDefaultAsync();
+            if (welcomeEdu == null)
+            {
+                return NotFound();
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View(welcomeEdu);
+            }
+
+            if(welcomeEdu.FormFile!= null)
+            {
+                if (!Helper.IsImage(welcomeEdu.FormFile))
+                {
+                    ModelState.AddModelError("Formfile", "File type must be image");
+                }
+                if (!Helper.IsSizeOk(welcomeEdu.FormFile, 2))
+                {
+                    ModelState.AddModelError("Formfile", "File size can not be more than 2 mb");
+                }
+                Helper.RemoveImage(_env.WebRootPath, "/img/about", updatedWelcomeEdu.Image);
+                updatedWelcomeEdu.Image = welcomeEdu.FormFile.CreateImage(_env.WebRootPath, "/img/about");
+            }
+
+            updatedWelcomeEdu.Title = welcomeEdu.Title;
+            updatedWelcomeEdu.Description = welcomeEdu.Description;
+            updatedWelcomeEdu.Image = welcomeEdu.Image;
+            updatedWelcomeEdu.UpdatedDate = DateTime.UtcNow.AddHours(4);
+            updatedWelcomeEdu.Link = welcomeEdu.Link;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- EduHome.Core/Entities/WelcomeEdu.cs
+++ EduHome.Core/Entities/WelcomeEdu.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,14 @@ namespace EduHome.Core.Entities
 {
     public class WelcomeEdu:BaseModel
     {
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public string? Link { get; set; }
+
+        [Required]
         public string? Image { get; set; }
 
         [NotMapped]
