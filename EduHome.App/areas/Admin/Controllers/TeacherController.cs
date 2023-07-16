e1e0464a286974f408ec80879a7b54cdc4a2b225
IsaSadigovDV <isans@code.edu.az>
2023-07-16 21:25:30 +0400

    teacher controller added

---
+++ EduHome.App/areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,47 @@
+﻿using EduHome.App.Context;
+using EduHome.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TeacherController : Controller
+    {
+        private readonly EduHomeDbContext _context;
+
+        public TeacherController(EduHomeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Teacher> teachers = await _context.Teachers
+                .Where(x => !x.IsDeleted)
+                .Include(x=>x.Position)
+                .Include(x=>x.Degree).ToListAsync();
+            return View(teachers);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            await _context.AddAsync(teacher);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
