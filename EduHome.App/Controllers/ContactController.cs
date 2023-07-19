a0d5562b06da258ec7b8bc7f5550a857f5092ee2
IsaSadigovDV <isans@code.edu.az>
2023-07-19 23:25:50 +0400

    contact messag view added

---
+++ EduHome.App/Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using EduHome.App.Context;
+using EduHome.App.ViewModels;
+using EduHome.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.App.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly EduHomeDbContext _context;
+
+        public ContactController(EduHomeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ContactVM contactVM = new ContactVM()
+            {
+                Setting = await _context.Settings.Where(x => !x.IsDeleted).FirstOrDefaultAsync()
+            };
+            return View(contactVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+
+            await _context.Contacts.AddAsync(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ EduHome.App/ViewModels/ContactVM.cs
@@ -0,0 +1,10 @@
+﻿using EduHome.Core.Entities;
+
+namespace EduHome.App.ViewModels
+{
+    public class ContactVM
+    {
+        public Setting Setting { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
